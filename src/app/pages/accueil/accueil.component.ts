cc2ad7ccb4452e1b89d63dba40b9c57ec55209fd
keurbi <desousa.loic8@gmail.com>
2024-11-25 15:30:23 +0100

    boutons

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.scss',
 })
 export class AccueilComponent {
-  compteur = 0;
+  saisieImage = '';
 
-  listeCategories = ['S', 'A', 'B', 'D', 'E'];
+  listeCategories = [
+    {
+      titre: 'Super',
+      images: [
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoUbGxdooqKHGls6i0vJT0yXIIHN-PJtllBw&s',
+        'https://assets.evcdn.net/dam-images/83712/16-9/750x422.jpeg',
+      ],
+    },
+    { titre: 'Bien', images: [] },
+    { titre: 'Moyen', images: [] },
+    { titre: 'Bof', images: [] },
+  ];
 
-  onClick() {
-    this.compteur++;
+  ajoutImage() {
+    if (this.saisieImage != '') {
+      this.listeCategories[0].images.push(this.saisieImage);
+      this.saisieImage = '';
+    }
   }
 }
