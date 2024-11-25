4c1674967bf0fac2d824a476a630a69367de8fa4
keurbi <desousa.loic8@gmail.com>
2024-11-25 11:29:12 +0100

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { ConnexionComponent } from './pages/connexion/connexion.component';
+import { Page404Component } from './pages/page404/page404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "Accueil", component: AccueilComponent },
+    {path: "Connexion", component: ConnexionComponent },
+    {path: "", redirectTo: "Accueil", pathMatch: "full"},
+    {path: "**", component: Page404Component},
+]; 
