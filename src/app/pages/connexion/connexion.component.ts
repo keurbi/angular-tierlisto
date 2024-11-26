359101145b57742b7f8d4245dbee3b4c83b42d69
keurbi <desousa.loic8@gmail.com>
2024-11-26 10:46:25 +0100

    CONNECT

--- src/app/pages/connexion/connexion.component.ts
+++ src/app/pages/connexion/connexion.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-connexion',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.scss',
 })
-export class ConnexionComponent {}
+export class ConnexionComponent {
+  formBuilder = inject(FormBuilder);
+
+  formulaire = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+}
